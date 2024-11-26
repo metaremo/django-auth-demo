ee0e00834dbc4155b2c924c09469380596a21536
meta remote <metaremote7@gmail.com>
2024-11-26 14:18:20 +0700

    2.add template + accounts

--- moziauth/urls.py
+++ moziauth/urls.py
@@ -19,7 +19,14 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    #path('', include('accounts.urls')),
+    #path('accounts/', include('django.contrib.auth.urls')),  #logged in and authorise the user --> need to auth
+    #path('members/', include('members.urls')),
+    #path('stores/', include('stores.urls')),
+    #path('manufacturers/', include('manufacturers.urls')),
+
+    path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
+
+
 
 ]
